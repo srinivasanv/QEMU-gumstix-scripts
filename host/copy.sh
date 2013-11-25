c2987d2bda7b3ea962fffd4d0b058ced8af4e319
Srinivasan Vijayaraghavan <srinivav@andrew.cmu.edu>
2013-11-25 00:47:07 -0500

    Added copy script

---
+++ host/copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change this directory for each lab
+sudo cp ~/18349/lab4/tasks/bin/*.bin /media/bootfs/
+sudo cp ~/18349/lab4/kernel/kernel.bin /media/bootfs/
+MSG="Copied kernel/kernel.bin and tasks/bin files"
+echo $MSG
+
